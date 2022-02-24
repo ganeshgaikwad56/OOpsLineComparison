5c9915c7cba8fbfd027a40fcf4ab95cb4f1a0dec
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-02-24 23:50:24 +0530

    Pushing Greeting Message to Line Comparison

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Oops1EmployeeWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Program");
+        }
+    }
+}
